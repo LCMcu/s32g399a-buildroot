ccc9c88002776f514d021944bad3f4721ac176c0
LCMcu <1771621931@qq.com>
2025-09-24 03:29:02 -0700

    rm init_driver.sh gpio init

--- board/s32g/usr/app/start_script/init_driver.sh
+++ board/s32g/usr/app/start_script/init_driver.sh
@@ -8,28 +8,28 @@ insmod /lib/modules/6.6.52-rt43-1.0+/kernel/drivers/mailbox/llce-mailbox.ko
 insmod /lib/modules/6.6.52-rt43-1.0+/kernel/drivers/mfd/llce-core.ko
 insmod /lib/modules/6.6.52-rt43-1.0+/kernel/drivers/net/can/llce/llce_can.ko
 
-#######################################配置外部输入GPIO###########################################
-#外部输入GPIO C15 PJ_04
-echo 148 > /sys/class/gpio/export
-echo in > /sys/class/gpio/PJ_04/direction
+# #######################################配置外部输入GPIO###########################################
+# #外部输入GPIO C15 PJ_04
+# echo 148 > /sys/class/gpio/export
+# echo in > /sys/class/gpio/PJ_04/direction
 
-#外部输入GPIO D15 PJ_06
-echo 150 > /sys/class/gpio/export
-echo in > /sys/class/gpio/PJ_06/direction
+# #外部输入GPIO D15 PJ_06
+# echo 150 > /sys/class/gpio/export
+# echo in > /sys/class/gpio/PJ_06/direction
 
-#外部输入GPIO F16 PJ_08
-echo 152 > /sys/class/gpio/export
-echo in > /sys/class/gpio/PJ_08/direction
+# #外部输入GPIO F16 PJ_08
+# echo 152 > /sys/class/gpio/export
+# echo in > /sys/class/gpio/PJ_08/direction
 
-#外部输入GPIO D16 PJ_10
-echo 154 > /sys/class/gpio/export
-echo in > /sys/class/gpio/PJ_10/direction
+# #外部输入GPIO D16 PJ_10
+# echo 154 > /sys/class/gpio/export
+# echo in > /sys/class/gpio/PJ_10/direction
 
-#######################################读取GPIO状态##############################################
-cat /sys/class/gpio/PJ_04/value
-cat /sys/class/gpio/PJ_06/value
-cat /sys/class/gpio/PJ_08/value
-cat /sys/class/gpio/PJ_10/value
+# #######################################读取GPIO状态##############################################
+# cat /sys/class/gpio/PJ_04/value
+# cat /sys/class/gpio/PJ_06/value
+# cat /sys/class/gpio/PJ_08/value
+# cat /sys/class/gpio/PJ_10/value
 
 #######################################CAN配置###################################################
 ip link set can0 type can bitrate 500000 loopback off
